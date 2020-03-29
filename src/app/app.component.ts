a1b6d27ba2617a0564f3c0574809031815083fa1
Dzaki Buchori <dzakibuchori@gmail.com>
2020-03-29 22:06:11 +0700

    update app component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {AuthService} from './services/auth.service';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,16 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angular-kasir';
+
+  constructor(
+    private authService: AuthService
+  ) {}
+
+  logout() {
+    this.authService.logout();
+  }
+
+  isLogin() {
+    return this.authService.isLogin();
+  }
 }
