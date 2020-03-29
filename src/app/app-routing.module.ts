0b4261d70ac7851471217e3c844502cefe93e4f4
Dzaki Buchori <dzakibuchori@gmail.com>
2020-03-29 22:05:40 +0700

    update routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,32 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {LoginComponent} from './login/login.component';
+import {Error404Component} from './error404/error404.component';
+import {MenuComponent} from './menu/menu.component';
+import {PaymentComponent} from './payment/payment.component';
+import {AuthGuard} from './auth/auth.guard';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: MenuComponent,
+    canActivate: [AuthGuard]
+  },
+  {
+    path: 'login',
+    component: LoginComponent,
+  },
+  {
+    path: 'payment',
+    component: PaymentComponent,
+    canActivate: [AuthGuard]
+  },
+  {
+    path: '**',
+    component: Error404Component,
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
